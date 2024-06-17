77b1175bf800f2b2c6dbc80abbac849fbbb56334
ecolus <ecolus48@gmail.com>
2024-06-17 15:43:02 +0100

    Submit button now transitions to game screen.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 
-let username = ''
+let username = 'Adam'
 let game = {
-    dictionary: ["Bramble"],
+    dictionary: ["bramble"],
     currentWord: [],
+    hiddenWord: [],
     guesses: 0,
-    score: 0,
-    guesses: 0
+    score: 0
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -13,10 +13,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                username = collectUsername();
+            if (this.getAttribute("type") === "submit") {
+                //collectUsername();
                 console.log(`Get ready to play ${username}!`);
                 runGame();
+                console.log(`the word for this game is ${game["dictionary"]}`)
             }
         });
     }
@@ -26,21 +27,43 @@ document.addEventListener("DOMContentLoaded", function() {
  * The collectUsername function servers to take the input data from the user-name input box and transfer it to the username variable so that it
  * can be used to tell the play whether they've won or lost in the game of hangman.
  */
-function collectUsername(){
-    const userInput = document.getElementById("user-name");
-    return userInput.value;
-}
-
+//function collectUsername(){
+    //const userInput = document.getElementById("user-name");
+   // if (userInput !== null){
+       // username = userInput.value;
+   // } else {
+      //  console.error("Please insert a valid username!")
+      //  throw `Invalid User Input ${userInput}. Aborting!`;
+    //}
+//}
+/***
+ * The runGame function is in place to set up both the HTML required to run the game but also the variables nessecary for elements to display properly
+ * such as the current word the game is using and the _ lines beneath the hangman image showing the current player guesses and their progress in the game.
+ */
 function runGame(){
     console.log("Running the game...")
-    //dictionary = words[Math.floor(Math.random() * 1175)];
-    //document.getElementById("user-header").innerText = `<img src="/assets/images/apfel.jpg" alt="0/6 Guesses remaining">`;
-    //document.getElementById("user-info").innerText = `<div id="current-word">_ _ _ _ _ _ _</div>`;
+    currentWord = game["dictionary"]
+    //currentWord = words[Math.floor(Math.random() * 1175)];
+    gameScreenEle = `<img id="game-screen" src="assets/images/apfel.jpg" alt="0/6 Guesses remaining">`
+    currentWordEle = `<div id="current-word"></div>`
+    document.getElementById("score").innerHTML = "Score: 0"
+    document.getElementById("user-header").outerHTML = gameScreenEle;
+    document.getElementById("user-info").outerHTML = currentWordEle;
+    document.getElementsByName
 
-}
+        if (currentWord !== null){
+            let word = currentWord[0]
+            hiddenWord = ' _ '.repeat(word.length)
+            document.getElementById('current-word').innerHTML = hiddenWord
+        } else {
+            console.error("An invalid word has been chosen!")
+            throw `Invalid hangman word chosen: ${currentWord}. Aborting!`;
+        }
+    }
+
 
 function checkLetter(){
-    //let letterSuggestion = document.getElementById("user-name").value
+    //let letterSuggestion = document.getElementById("letter-input").value
     for (let letters of currentWord) {
         if (letters = letterSuggestion){
             updateHiddenWord()
