d4f912b0f4f7982eb91dce82e61152414c1dc4b0
ecolus <ecolus48@gmail.com>
2024-06-19 18:59:50 +0100

    Fixed issues with .methods in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
-let username = 'Adam'
+let username = ''
 let game = {
-    dictionary: ["bramble"],
+    dictionary: words,
     currentWord: '',
     hiddenWord: '',
     guessedWords: [],
@@ -16,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("type") === "submit" && game["gameStart"] == 0) {
-                //collectUsername();
-                console.log(`Get ready to play ${username}!`);
-                console.log(`the word for this game is ${game["dictionary"]}`)
+                collectUsername();
+                alert(`Get ready to play ${username}!`);
+                console.log(`the word for this game is ${game["currentWord"]}`)
                 ++game["gameStart"]
                 changeHTMLForGameplay()
                 runGame();
@@ -33,15 +33,16 @@ document.addEventListener("DOMContentLoaded", function() {
  * The collectUsername function servers to take the input data from the user-name input box and transfer it to the username variable so that it
  * can be used to tell the play whether they've won or lost in the game of hangman.
  */
-//function collectUsername(){
-    //const userInput = document.getElementById("user-name");
-   // if (userInput !== null){
-       // username = userInput.value;
-   // } else {
-      //  console.error("Please insert a valid username!")
-      //  throw `Invalid User Input ${userInput}. Aborting!`;
-    //}
-//}
+function collectUsername(){
+    const userInput = document.getElementById("user-name");
+    username = userInput.value;
+    if (userInput !== null){
+        username = userInput.value;
+    } else {
+        console.error("Please insert a valid username!")
+        throw `Invalid User Input ${userInput}. Aborting!`;
+    }
+}
 
 function changeHTMLForGameplay(){
 
@@ -58,16 +59,18 @@ function changeHTMLForGameplay(){
  * such as the current word the game is using and the _ lines beneath the hangman image showing the current player guesses and their progress in the game.
  */
 function runGame(){
-    game.guesses = 0
-    document.getElementById("game-screen").outerHTML = `<img id="game-screen" src="assets/images/0-guesses.jpg" alt="0/6 Guesses remaining"></img>`
-    console.log("Running the game...")
-    currentWord = game["dictionary"]
+    game.guesses = 0;
+    document.getElementById("game-screen").outerHTML = `<img id="game-screen" src="assets/images/0-guesses.jpg" alt="0/6 Guesses remaining"></img>`;
+    console.log("Running the game...");
+    currentWord = words[Math.floor(Math.random() * 1175)];
+    alert(currentWord);
     //currentWord = words[Math.floor(Math.random() * 1175)];
 
     if (currentWord !== null){
-        let word = currentWord[0]
-        hiddenWord = ' _ '.repeat(word.length)
-        document.getElementById('current-word').innerHTML = hiddenWord
+        let spaces = ' _ '
+        game.hiddenWord = spaces.repeat(currentWord.length);
+        //game.hiddenWord = ' _ ' * word.length;
+        document.getElementById('current-word').innerHTML = game.hiddenWord;
         } else {
             console.error("An invalid word has been chosen!")
             throw `Invalid hangman word chosen: ${currentWord}. Aborting!`;
@@ -81,14 +84,14 @@ function checkLetter(){
     //game.currentWord[0].split('') This is to divide up the currentWord into individual letters that can be searched through with a for loop.
     let correctWord = 0
     let incorrectWord = 0
-    let letterSuggestion = 'b'
-    let wordArray = ['b', 'r', 'a', 'm', 'b', 'l', 'e'];
+    let letterSuggestion = document.getElementById("user-name").value
+    let wordArray = game.currentWord[0].split('');
 
-    for (let guess in game.guessedWords){
-        if (letterSuggestion == guess){
-            alert("You have already used this word!")
-        }
-    }
+    //for (let guess in game.guessedWords){
+        //if (letterSuggestion == guess){
+            //alert("You have already used this word!")
+        //}
+    //}
 
     for (let letters of wordArray) {
         if (letterSuggestion === letters){
